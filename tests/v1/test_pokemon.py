53391d5b72367baee68e56228e2ad435445e8562
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2021-04-10 16:39:17 +0100

    added full-circle test

--- tests/v1/test_pokemon.py
+++ tests/v1/test_pokemon.py
@@ -5,25 +5,27 @@ from main import APP
 client = TestClient(APP)
 
 
-def test__get_pokemon_empty():
+def test__GET_pokemon_empty():
     response = client.get("/pokemon")
 
     assert response.status_code == 404
     assert response.json()["detail"] == "Not Found"
 
 
-def test__get_pokemon__empty(mock_network):
-    response = client.get("/pokemon")
-
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Not Found"
-
-
-def test__get_pokemon__not_a_pokemon(mock_network):
+def test__GET_pokemon__not_a_pokemon(mock_network):
     response = client.get("/pokemon/not_a_pokemon")
 
     assert response.status_code == 404
     assert response.json()["detail"] == (
         f"`not_a_pokemon` could not be found in national pokedex, send a "
         "live specimen if this is a mistake."
+    )
+
+def test__GET_pokemon__bulbasaur(mock_network):
+    response = client.get("/pokemon/bulbasaur")
+
+    assert response.status_code == 200
+    assert response.json()["description"] == (
+        "Thither is a seed on its back. By soaking up the travelling lamp’s "
+        "rays,  the seed."
     )
